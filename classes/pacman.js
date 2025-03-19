6d688ae2780e3e1988b04894b67890832f7c449a
Giorgia Cavalleri (Student) <N00230352@iadt.ie>
2025-03-19 12:15:27 +0000

    packman figure

---
+++ classes/pacman.js
@@ -0,0 +1,18 @@
+class Pacman{
+    constructor(obj){
+        this.posX = obj.posX || 100;
+		this.posY = obj.posY || 100;
+    }
+
+    render(){
+        push()
+        translate (this.posX, this.posY)
+
+        noStroke();
+        fill(250, 222, 17);
+        arc(150, 200, 50, 50, 225, 135); 
+
+        pop()
+    }
+    
+}
--- sketch.js
+++ sketch.js
@@ -1,9 +1,13 @@
+let pacman;
 
 function setup(){
     createCanvas(500,500);
     angleMode(DEGREES);
+
+    pacman = new Pacman({});
 }
 
 function draw(){
     background(52,52,52)
+    pacman.render();
 }
