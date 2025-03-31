3a16ae824586f71039a567173155bf92b637f5d6
giorgia <n00230352@iadt.ie>
2025-03-31 22:25:04 +0100

    movemnet of ghost up, down, left and right in arena

--- classes/Ghost.js
+++ classes/Ghost.js
@@ -1,19 +1,49 @@
-class Ghost{
-    constructor(){
-        this.posX = random(0,300)
-        this.posY = random(0,300)
+class Ghost {
+    constructor(obj = {}) { 
+        this.posX = obj.posX || random(50, 450); 
+        this.posY = obj.posY || random(50, 450);
+
+        this.speed = obj.speed || 2; 
+        
+        // Only move up/down or left/right (no diagonal movement)
+        let directions = [
+            createVector(1, 0),  
+            createVector(-1, 0), 
+            createVector(0, 1),  
+            createVector(0, -1)  
+        ];
+        this.direction = random(directions);
+
+        this.color = color(random(255), random(255), random(255));
     }
 
-    render(){
-        push()
-        translate(this.posX, this.posY)
+    move() {
+        this.posX += this.speed * this.direction.x;
+        this.posY += this.speed * this.direction.y;
+
+        if (this.posX <= 50) {
+            this.direction.x = 1; 
+        } 
+        if (this.posX >= 450) {
+            this.direction.x = -1; 
+        } 
         
-        noLoop();
+        if (this.posY <= 50) {
+            this.direction.y = 1; 
+        } 
+        if (this.posY >= 450) {
+            this.direction.y = -1; 
+        } 
+    }
+
+    render() {
+        push();
+        translate(this.posX, this.posY);
         noStroke();
-        fill(random(255), random(255), random(255));
-        // rect(100, 100, 25, 12.5); 
-        ellipse(112.5, 100, 25, 25);  
         
-        pop()
+        fill(this.color);
+        ellipse(0, 0, 15, 15);  
+
+        pop();
     }
 }
--- sketch.js
+++ sketch.js
@@ -1,26 +1,28 @@
 let pacman;
+let myArena;
 let ghosts = [];
-let numGhosts = 12;
 
 function setup(){
     createCanvas(500,500);
     angleMode(DEGREES);
 
     pacman = new Pacman({});
+    myArena = new Arena({});
 
-    for(let i = 0; i<numGhosts; i++){
-        ghosts.push((new Ghost));
-        }
+    for (let i = 0; i < 5 ; i++) { 
+        ghosts.push(new Ghost({})); 
+    }
 }
 
 function draw(){
     background(52,52,52)
-    pacman.render();
-    pacman.move();
+    myArena.renderArena();
 
-    // for(let i = 0; i<numGhosts; i++){
-    //     ghosts[i].render();
-    //     }
+    for (let i = 0; i < ghosts.length; i++) {
+        ghosts[i].move();
+        ghosts[i].render(); 
+    }
+
 } 
 
 
